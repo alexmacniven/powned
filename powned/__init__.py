64eb9ea307e5467e0ffcd91ff84b56bfd5ec0f0b
Alex Macniven <macniven.ap@gmail.com>
2019-10-30 21:24:03 +0000

    Adds Package Docstring

--- powned/__init__.py
+++ powned/__init__.py
@@ -1,6 +1,14 @@
 """
-package
+powned
+======
 
-A python package template.
+Powned is a python package allowing you to check if your password has
+been recovered in a data breach.
+
+Basic usage:
+
+    >>> import powned
+    >>> powned.check('password1')
+    2413945
 """
 from .api import check  # noqa F401
